d162a7211e9caafb02ee0bd1bbf83ce1cced2501
potsawat <potsawat_3232@hotmail.com>
2018-12-09 19:02:50 +0700

    create api customers_list

---
+++ api/controllers/customers_list.controller.js
@@ -0,0 +1,57 @@
+const db = require('../index')
+
+exports.findAll = (req, res) => {
+    db.customersList.findAll({include: [ { model: db.typeStock, as: 'type' }] } ).then(stock => res.json(stock))
+}
+
+
+//Create
+exports.create = (req, res) => {
+    //Id is auto increment
+    db.customersList.create({  
+            amount: req.body.amount,
+            total: req.body.total,
+            typeStock_id : req.body.typeStock_id
+        })
+        .then(data => {		
+            res.json(data);
+        })
+        .catch(error => res.status(400).send(error))
+}
+
+//Find by Id
+exports.findById = (req, res) => {
+
+    db.customersList.findById(req.params.Id)
+    .then(stock => {
+            if (!stock){
+                return res.status(404).json({message: "Stock Not Found"})
+            }
+            return res.status(200).json(stock)
+        }
+    )
+    .catch(error => res.status(400).send(error));
+}
+
+exports.update = (req, res) => {
+	return db.customersList.findById(req.params.Id)
+		.then(
+			stock => {
+				if(!stock){
+					return res.status(404).json({
+						message: 'stock Not Found',
+					});
+				}
+				return stock.update({
+                                amount: req.body.amount,
+                                total: req.body.total,
+                                typeStock_id : req.body.typeStock_id
+									},{ 
+                                        where: { id: req.params.id } 
+                                    })
+									.then(() => res.status(200).json(stock))
+									.catch((error) => res.status(400).send(error));
+				}
+			)
+		.catch((error) => res.status(400).send(error));			 
+};
